172365f4637441bdaff3f569fc4fbfd2ba23cf20
Vicky Bikia <vicky@tsf-460-wpa-3-210.epfl.ch>
2018-03-13 15:15:09 +0100

    hard coded lambda

--- sampling_functions.m
+++ sampling_functions.m
@@ -1,34 +1,34 @@
-function t_sampled = sample_business(t,lambda,lambda_max)
-    t_sampled = Nan
+function t_sampled = sample_business(t)
+    t_sampled = Nan;
     while t_sampled == Nan
-        r=rand();
-        t=t+log(r)/lambda_max;
-        s=rand();
-        if s<=lambdaFunction(t)/lambda
+        r = rand();
+        t = t+log(r)/1.2;
+        s = rand();
+        if s<=sin(degtorad(t))
             t_sampled = t;
         end
     end
 end
 
-function t_sampled = sample_leisure(t,lambda,lambda_max)
-    t_sampled = Nan
+function t_sampled = sample_leisure(t)
+    t_sampled = Nan;
     while t_sampled == Nan
-        r=rand();
-        t=t+log(r)/lambda_max;
-        s=rand();
-        if s<=lambdaFunction(t)/lambda
+        r = rand();
+        t = t+log(r)/0.6;
+        s = rand();
+        if s<=t/179
             t_sampled = t;
         end
     end
 end
 
-function t_sampled = sample_economy(t,lambda,lambda_max)
-    t_sampled = Nan
+function t_sampled = sample_economy(t)
+    t_sampled = Nan;
     while t_sampled == Nan
-        r=rand();
-        t=t+log(r)/lambda_max;
-        s=rand();
-        if s<=lambdaFunction(t)/lambda
+        r = rand();
+        t = t+log(r)/1.6;
+        s = rand();
+        if s<= 0.5*(1+sin(degtorad(t+180)))
             t_sampled = t;
         end
     end
