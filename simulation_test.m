aadaef5fe475f85ec56c271765037039cfa8d6ff
Dimitrios Tsitsokas <dtsitsokas@gmail.com>
2018-04-09 13:33:57 +0200

    Statistics for couldnt_purchase

--- simulation_test.m
+++ simulation_test.m
@@ -14,15 +14,18 @@ verbosity = false;
 scenario = NewFlight();
 
 % Run the simulation
-number_of_no_purchase = [];
-number_of_business_customers = [];
-number_of_leisure_customers = [];
-number_of_economy_customers = [];
-number_of_no_purchase_business = [];
-number_of_no_purchase_leisure = [];
-number_of_no_purchase_economy = [];
-number_of_seats_sold = [];
-sold_out_times = zeros(scenario.NUMBER_OF_PRODUCTS,1);
+number_of_couldnt_purchase=[];				%Total No of customers who CANNOT purchase (flight sold out) in each simulation test 
+number_of_no_purchase = [];					%Total No of customers who DO NOT purchase (dicide to do so) in each simulation test 
+number_of_business_customers = [];			%Total No of business customers who "arrive" in each simulation test  
+number_of_leisure_customers = [];			%Total No of leisure customers who "arrive" in each simulation test
+number_of_economy_customers = [];			%Total No of economy customers who "arrive" in each simulation test
+number_of_no_purchase_business = [];		%Total No of business customers who DO NOT purchase (decide to do so) in each simulation test 
+number_of_no_purchase_leisure = [];			%Total No of leisure customers who DO NOT purchase (decide to do so) in each simulation test
+number_of_no_purchase_economy = [];			%Total No of economy customers who DO NOT purchase (decide to do so) in each simulation test
+number_of_seats_sold = [];					%Total No of seats sold in each simulation test 
+
+
+sold_out_times = zeros(scenario.NUMBER_OF_PRODUCTS,1);  %Sold-out times for each product 
 
 total_number_of_customers = [];
 total_number_of_economy_customers = [];
@@ -30,12 +33,16 @@ total_number_of_economy_customers2 = [];
 total_revenue = [];
 
 
-max_number_of_no_purchase_avg = 0;      % mean value of the max number of no purchases
-max_number_of_no_purchase_var = 0;      % variance of the max number of no purchases
+%max_number_of_no_purchase_avg = 0;      % mean value of the max number of no purchases
+%max_number_of_no_purchase_var = 0;      % variance of the max number of no purchases
 no_purchase_avg = 0;
 no_purchase_var = 0;
+couldnt_purchase_avg=0;
+couldnt_purchase_var=0;
 list_no_purchase_avg = [];
 list_no_purchase_var = [];
+list_couldnt_purchase_avg = [];
+list_couldnt_purchase_var = [];
 
 run = 0;
 while(true)
@@ -49,24 +56,38 @@ while(true)
         
     total_number_of_economy_customers = [total_number_of_economy_customers, sum(segments==3)];
     
-    if sum(available_seats_for_fare(1:(end-1)))==0
+	%Check if flight is sold out or not - Count "Couldn't purchase" and "Didn't purchase" 
+    if sum(available_seats_for_fare(1:(end-1)))==0 %Case : Sold out 
         index = find(revenues,1,'last');   % last customer that could by a ticket, afterwards no seats were left
-        number_of_coundnt_purchase = [number_of_no_purchase, sum(revenues(index:end)==0)];
+        number_of_couldnt_purchase = [number_of_couldnt_purchase, sum(revenues(index:end)==0)];
         
         revenues = revenues(1:index);
         segments = segments(1:index);
-    else
-        number_of_coundnt_purchase = [number_of_no_purchase, 0];
+    else %Case Not sold out 
+        number_of_couldnt_purchase = [number_of_couldnt_purchase, 0];
     end
     
+	%No of passengers who "Didn't purchase" 
     number_of_no_purchase = [number_of_no_purchase, sum(revenues==0)];
-    
-    max_number_of_no_purchase = max(number_of_no_purchase); % [persons] maximum length of the number_of_no_purchase in this simulation run
-    [max_number_of_no_purchase_avg, max_number_of_no_purchase_var] = UpdatedStatistics(max_number_of_no_purchase_avg, max_number_of_no_purchase_var, max_number_of_no_purchase, run);
+	
+    % Worst case - all simlulations 
+    max_number_of_no_purchase = max(number_of_no_purchase); % [persons] maximum length of the number_of_no_purchase 
+	
+	
+    %[max_number_of_no_purchase_avg, max_number_of_no_purchase_var] = UpdatedStatistics(max_number_of_no_purchase_avg, max_number_of_no_purchase_var, max_number_of_no_purchase, run);
+   
+	%Statistics - Passengers who don't purchase
     [no_purchase_avg, no_purchase_var] = UpdatedStatistics(no_purchase_avg, no_purchase_var, number_of_no_purchase(end), run);
     list_no_purchase_avg = [list_no_purchase_avg, no_purchase_avg];
     list_no_purchase_var = [list_no_purchase_var, no_purchase_var];
     
+	%Statistics - Passengers who CANNOT purchase (Flight sold out) 
+	[couldnt_purchase_avg, couldnt_purchase_var] = UpdatedStatistics(couldnt_purchase_avg, couldnt_purchase_var, number_of_couldnt_purchase(end), run);
+	list_couldnt_purchase_avg = [list_couldnt_purchase_avg, couldnt_purchase_avg];
+    list_couldnt_purchase_var = [list_couldnt_purchase_var, couldnt_purchase_var];
+	
+	
+	
     number_of_business_customers = [number_of_business_customers, sum(segments==1)];
     number_of_leisure_customers = [number_of_leisure_customers, sum(segments==2)];
     number_of_economy_customers = [number_of_economy_customers, sum(segments==3)];
@@ -85,7 +106,7 @@ while(true)
         no_purchase_avg        
     end
     
-    if var(number_of_no_purchase)/run < 0.5 && var(number_of_no_purchase)/run > 0 && run >10
+    if var(number_of_no_purchase)/run < 0.5 && var(number_of_no_purchase)/run > 0
         break;
     end    
         
@@ -94,13 +115,16 @@ end
 fprintf('Number of simulation runs: %d\n', run)
 
 %% Plotting results 
+
+%Passengers who DON'T purchase 
+
 fprintf('No purchase');
 figure; histogram(number_of_no_purchase); grid on;
 title('Distribution of "No purchases"')
 xlabel('Number of "No purchases"'); ylabel('Frequency'); 
 mean_no_purchase = mean(number_of_no_purchase)
-MSE_no_purchase = var(number_of_no_purchase)/length(number_of_no_purchase)
-bootMSE_no_purchase = BootstrapMSE(number_of_no_purchase, @mean, 100)
+MSE_no_purchase = var(number_of_no_purchase)/length(number_of_no_purchase) %theoretical MSE of mean No-purchase
+bootMSE_no_purchase = BootstrapMSE(number_of_no_purchase, @mean, 100)	   %Bootstrapp MSE of mean No-purchase
 hold on;
 ylim=get(gca,'ylim');
 l1 = line([mean_no_purchase mean_no_purchase], ylim,'Color','r','LineWidth',2.0);
@@ -112,12 +136,34 @@ hold off;
 legend([l1,l2,l3,l4],'mean', 'MSE', 'bootstrap MSE','5 and 95 percentile')
 saveas(gcf,sprintf('no_purchase_scenario%i.png',scenario.CaseIndex));
 
+%Passengers who CANNOT purchase (Flight sold-out) 
+
+fprintf('Could not purchase');
+figure; histogram(number_of_couldnt_purchase); grid on;
+title('Distribution of "Could not purchase"')
+xlabel('Number of "Could not purchase" customers'); ylabel('Frequency'); 
+mean_couldnt_purchase = mean(number_of_couldnt_purchase)
+MSE_couldnt_purchase = var(number_of_couldnt_purchase)/length(number_of_couldnt_purchase) %theoretical MSE of mean Couldnt-purchase
+bootMSE_couldnt_purchase = BootstrapMSE(number_of_couldnt_purchase, @mean, 100)	   %Bootstrapp MSE of mean Couldnt-purchase
+hold on;
+ylim=get(gca,'ylim');
+l1 = line([mean_couldnt_purchase mean_couldnt_purchase], ylim,'Color','r','LineWidth',2.0);
+l2 = line([mean_couldnt_purchase+MSE_couldnt_purchase mean_couldnt_purchase+MSE_couldnt_purchase], ylim,'Color','r','LineWidth',2.0,'LineStyle',':');
+l3 = line([mean_couldnt_purchase-bootMSE_couldnt_purchase mean_couldnt_purchase-bootMSE_couldnt_purchase], ylim,'Color','r','LineWidth',2.0,'LineStyle','--');
+l4 = line([prctile(number_of_couldnt_purchase,5) prctile(number_of_couldnt_purchase,5)], ylim,'Color','g','LineWidth',2.0,'LineStyle','--');
+line([prctile(number_of_couldnt_purchase,95) prctile(number_of_couldnt_purchase,95)], ylim,'Color','g','LineWidth',2.0,'LineStyle','--');
+hold off;
+legend([l1,l2,l3,l4],'mean', 'MSE', 'bootstrap MSE','5 and 95 percentile')
+saveas(gcf,sprintf('couldnt_purchase_scenario%i.png',scenario.CaseIndex));
+
+%Total number of economy customer "arrivals" (Used as Y in variance reduction) 
 fprintf('Total number of Economy customers');
 figure; histogram(total_number_of_economy_customers); grid on;
 title('Distribution of the total number of Economy customers')
 xlabel('Total number of Economy customers [persons]'); ylabel('Frequency'); 
 saveas(gcf,sprintf('number_of_economy_customers_scenario%i.png',scenario.CaseIndex));
 
+%Variance Reduction for average no of no-purchase customers - Control Variates method 
 fprintf('Control variates no purchase');
 cov = 1/(length(number_of_no_purchase)-1)...
     *sum((number_of_no_purchase - mean(number_of_no_purchase))...
@@ -126,7 +172,7 @@ variance = 1/(length(number_of_no_purchase)-1)...
             *sum((total_number_of_economy_customers-mean(total_number_of_economy_customers)).^2);
 Z = number_of_no_purchase - cov/variance*(total_number_of_economy_customers- 144*(1-2/pi));
 
-
+%Plots Variance Reduction 
 figure; histogram(Z);
 title('Variance Reduction: Control Variates'); grid on;
 xlabel('Number of "No purchases"'); ylabel('Frequency'); 
@@ -145,13 +191,14 @@ line([prctile(Z,95) prctile(Z,95)], ylim,'Color','g','LineWidth',2.0,'LineStyle'
 hold off;
 legend([l1,l2,l3,l4],'mean', 'MSE', 'bootstrap MSE', '5 and 95 percentile')
 
+%Plot - Total Revenue 
 fprintf('Total revenue:')
 figure; histogram(total_revenue/10^6); grid on;
 title('Total revenue distribution'); 
 xlabel('Total revenue [millions of CHF]'); ylabel('Frequency');
-mean_total_revenue = mean(total_revenue/10^6)
-MSE_total_revenue = var(total_revenue/10^6)/length(total_revenue)
-bootMSE_total_revenue = BootstrapMSE(total_revenue/10^6, @mean, 100)
+mean_total_revenue = mean(total_revenue)/10^6
+MSE_total_revenue = var(total_revenue)/length(total_revenue)/10^6
+bootMSE_total_revenue = BootstrapMSE(total_revenue, @mean, 100)/10^6
 hold on;
 ylim=get(gca,'ylim');
 l1 = line([mean_total_revenue mean_total_revenue], ylim,'Color','r','LineWidth',2.0);
@@ -177,7 +224,7 @@ set(h, 'Box', 'off');
 set(h, 'Location', 'best');
 saveas(gcf,sprintf('stopping_criteria_scenario%i.png',scenario.CaseIndex));
 
-%Joined Graph for variance reduction (Z and Y=No of no purchases) 
+%Joined Graph for variance reduction (Z and X=No of no purchases) 
 figure; 
 joinedh = histogram(number_of_no_purchase);
 hold on;
@@ -186,7 +233,7 @@ hold off;
 grid on;
 xlabel('');
 ylabel('Frequency');
-h = legend('Y = # of no purchase', 'Variable Z');
+h = legend('X = # of no purchase', 'Variable Z');
 set(h, 'Box', 'off');
 set(h, 'Location', 'best');
 saveas(gcf,sprintf('variance_reduction_combined_scenario%i.png',scenario.CaseIndex));
