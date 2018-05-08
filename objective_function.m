338e1d00eefaa915af15fa4c1cff0059951eeb3a
Vicky Bikia <vicky@tsf-460-wpa-1-071.epfl.ch>
2018-05-08 17:04:36 +0200

    objective fun update

--- objective_function.m
+++ objective_function.m
@@ -1,16 +1,25 @@
-function [total_revenue, avg_available_seats_for_fare, avg_sold_out_time] = objective_function(scenario)
+function [total_revenue, avg_available_seats_for_fare, avg_sold_out_time, avg_number_of_no_purchase, avg_number_of_sales_for_each_fare] = objective_function(scenario)
 
     nruns = 1;
     total_revenues = zeros(nruns,1);
     all_available_seats_for_fare = zeros(nruns,scenario.NUMBER_OF_PRODUCTS);
     all_sold_out_time = zeros(nruns,scenario.NUMBER_OF_PRODUCTS);
+    number_of_no_purchase = zeros(1,nruns);
+    
     parfor i = 1:nruns
         [~, revenues, available_seats_for_fare, ~, sold_out_time] = simulation(scenario);
         total_revenues(i) = sum(revenues);
         all_available_seats_for_fare(i,:) = available_seats_for_fare;
         all_sold_out_time(i,:) = sold_out_time;
+        number_of_no_purchase = [number_of_no_purchase, sum(revenues==0)];
+        for k=1:1:scenario.NUMBER_OF_PRODUCTS
+            number_of_sales_for_each_fare(i,k) = length(revenues(revenues==scenario.revenues(k)));
+        end
     end
+    
     total_revenue = sum(total_revenues)/nruns;
     avg_available_seats_for_fare = sum(all_available_seats_for_fare,1)/nruns;
     avg_sold_out_time = sum(all_sold_out_time,1)/nruns;
+    avg_number_of_no_purchase = sum(number_of_no_purchase)/nruns;
+    avg_number_of_sales_for_each_fare = sum(number_of_sales_for_each_fare)/nruns; % how many times each fare was sold
 end
