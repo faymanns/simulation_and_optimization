e2903c679ffe9da4bfc638feef543fd6f1555074
faymanns <florian.aymanns@epfl.ch>
2018-05-08 17:09:35 +0200

    termination criterium

--- local_search.m
+++ local_search.m
@@ -4,11 +4,17 @@ function [solutions, values] = local_search(problem)
     solutions = [scenario];
     values = [problem.OBJECTIVE_FUNCTION(scenario)];
     
-    for i =1:10
+    termination_theshold = 10; %If no better solution is found for termination_theshold consecutive iteration the loop stops
+    number_of_no_change_iteration = 0;
+    
+    while number_of_no_change_iteration < termination_theshold
         [Q, avg_available_seats_for_fare, avg_sold_out_time] = problem.OBJECTIVE_FUNCTION(scenario);
         if Q > values(end)
             values = [values, Q];
             solutions = [solutions, scenario];
+            number_of_no_change_iteration = 0;
+        else
+            number_of_no_change_iteration = number_of_no_change_iteration + 1;
         end
         scenario = generate_neighbor_random(solutions(end), avg_available_seats_for_fare, avg_sold_out_time);
     end
