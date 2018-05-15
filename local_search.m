7ccc78b428d9934e1303aab30bde77a192fbd364
faymanns <florian.aymanns@epfl.ch>
2018-05-15 13:59:02 +0200

    maximum number of iterations

--- local_search.m
+++ local_search.m
@@ -4,10 +4,12 @@ function [solutions, values] = local_search(problem)
     solutions = [scenario];
     values = [problem.OBJECTIVE_FUNCTION(scenario)];
     
+    max_number_of_iterations = 20;
+    number_of_iteration = 0;
     termination_theshold = 10; %If no better solution is found for termination_theshold consecutive iteration the loop stops
     number_of_no_change_iteration = 0;
     
-    while number_of_no_change_iteration < termination_theshold
+    while number_of_no_change_iteration < termination_theshold && number_of_iteration < max_number_of_iterations
         [Q, avg_available_seats_for_fare, avg_sold_out_time] = problem.OBJECTIVE_FUNCTION(scenario);
         if Q > values(end)
             values = [values, Q];
@@ -16,6 +18,7 @@ function [solutions, values] = local_search(problem)
         else
             number_of_no_change_iteration = number_of_no_change_iteration + 1;
         end
+        number_of_iteration = number_of_iteration + 1;
         scenario = generate_neighbor_random(solutions(end), avg_available_seats_for_fare, avg_sold_out_time);
     end
 end
