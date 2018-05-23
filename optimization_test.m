399c566b8e24d5f6db7ca7ee1f99c878695e4ece
faymanns <florian.aymanns@epfl.ch>
2018-05-23 17:43:15 +0200

    improved plot for simulated annealing

--- optimization_test.m
+++ optimization_test.m
@@ -43,7 +43,7 @@ problem.OBJECTIVE_FUNCTION = @objective_function;
 %% Simulated Annealing
 problem.M = 10;
 problem.K = 50;
-problem.D = 0.5;
+problem.D = 1100;
 problem.P0 = 0.99;
 problem.Pf = 0.01;
 
@@ -54,10 +54,25 @@ save('simulated_annealing','solutions','values','temperatures');
 
 plot_solution(solutions(end),'simulated_annealing_optimal_solution.pdf');
 plot_occupancy(solutions(end),'simulated_annealing_occupancy.pdf');
+maxs = [values(1)];
+for i = values(2:end)
+    if maxs(end)<=i
+        maxs=[maxs,i];
+    else
+        maxs=[maxs,maxs(end)];
+    end
+end
 figure();
+yyaxis left;
 plot(values);
-xlabel('Iteration');
+hold on;
+plot(maxs);
+hold off;
 ylabel('Objective function');
+yyaxis right;
+plot(temperatures)
+xlabel('Iteration');
+legend(['x_c';'x* ';'T  '])
 
 
 end
